6bf948988807a29ec9a1cad8c31c8cd78f124ea9
Ryan Hamblin <ryanhca@gmail.com>
2014-11-13 13:49:13 -0700

    API setup with Kimono

--- public/js/myCtrl.js
+++ public/js/myCtrl.js
@@ -1,8 +1,8 @@
 var app = angular.module('fantasyApp');
 
-app.controller('myCtrl', function($scope){
+app.controller('myCtrl', function($scope, myService){
 
 	$scope.test = "Fantasy Football Finder";
-
+	$scope.playerList = myService.getPlayers();
 
 })
--- public/js/myService.js
+++ public/js/myService.js
@@ -1,1 +1,15 @@
 var app = angular.module('fantasyApp');
+
+app.factory('myService', function($http){
+
+	var factoryReturn = {};
+
+	var kimonoApiUrl = "https://www.kimonolabs.com/api/5j5mfsb0?apikey=3zfzqMzsUAvKEzb4IKLKl0y0Iv0qmyYD&callback=JSON_CALLBACK";
+
+	factoryReturn.getPlayers = function() {
+		return $http.jsonp(kimonoApiUrl).then(function(response){
+			return response.data
+		})   
+	}
+		return factoryReturn;
+});
